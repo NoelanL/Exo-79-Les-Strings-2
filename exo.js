2330e2567926047a2ed46efc37c5e593a419400d
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-24 16:45:06 +0100

    Exo 79

--- exo.js
+++ exo.js
@@ -1,3 +1,23 @@
 let pomme = "Elle est belle ma pomme !";
 
 console.log(pomme.lastIndexOf('m'));
+
+let exo1 = pomme.indexOf('pomme');
+console.log(exo1);
+
+if (pomme.startsWith("Elle")){
+    alert('Elle est belle ma pomme ! Commence par "Elle"');
+}
+
+if (pomme.endsWith("!")){
+    alert("Elle est belle ma pomme ! Se termine par un point d'exclamation");
+}
+
+let motPomme = pomme.substring(18, 23);
+let paraPomme = document.getElementById('pomme');
+paraPomme.innerHTML = motPomme;
+
+for (let i = 0; i < pomme.length; i++){
+    let oneLine = document.getElementById('one-line');
+    oneLine.innerHTML = pomme[i];
+}
